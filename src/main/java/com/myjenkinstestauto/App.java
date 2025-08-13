45ab91a04ace8cbdfbbd52a3c66575098a0e9da9
upadhyay-rajesh <upadhyay.rajesh@rediffmail.com>
2025-08-13 22:45:41 +0530

    auto deploy happen

--- src/main/java/com/myjenkinstestauto/App.java
+++ src/main/java/com/myjenkinstestauto/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Wsssxdvfcgvorld!sZxdcfvbnm" );
+        System.out.println( "Hello Wsssxdvfcgvorld!sZxdcfvbzsdvxfcbgvnnm" );
     }
 }
